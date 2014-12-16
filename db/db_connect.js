8fdb7632242983fd844c354d70ffc35e6c17ff16
aisensiy <aisensiy@gmail.com>
2014-12-16 10:36:56 +0800

    add db_connect method

---
+++ db/db_connect.js
@@ -0,0 +1,16 @@
+var config = require('../config/config');
+var mongoose = require('mongoose');
+
+var connect = function () {
+  mongoose.connect('mongodb://' + config.get('DB_HOST') + ':' + config.get('DB_PORT') + '/' + config.get('DB_NAME'));
+}
+connect();
+
+var db = mongoose.connection;
+db.on('open', function callback () {
+  console.log('connected to mongodb');
+});
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('disconnected', connect);
+
+module.exports = mongoose;
