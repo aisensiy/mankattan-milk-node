b8c247316928ade4bc0fcc95d3c83123b734f177
aisensiy <aisensiy@gmail.com>
2014-12-17 21:13:16 +0800

    comment code to communicate

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,14 +11,14 @@ function get_code() {
 }
 
 $(function() {
-  var code = get_code();
-  var user = null;
-  $.get('/users/get?code=' + (code || ''), function(result) {
-    if (result.ret == 1 && result.url) {
-      window.location.href = window.location.origin + result.url;
-    } else if (result.user) {
-      user = result.user;
-      $('body').html(user.nickname);
-    }
-  });
+//  var code = get_code();
+//  var user = null;
+//  $.get('/users/get?code=' + (code || ''), function(result) {
+//    if (result.ret == 1 && result.url) {
+//      window.location.href = window.location.origin + result.url;
+//    } else if (result.user) {
+//      user = result.user;
+//      $('body').html(user.nickname);
+//    }
+//  });
 });
