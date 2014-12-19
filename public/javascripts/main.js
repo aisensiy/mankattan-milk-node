c483558460ee69eecbc7550ea38a46260e49a88b
aisensiy <aisensiy@gmail.com>
2014-12-19 12:26:36 +0800

    cow click animation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -36,6 +36,17 @@ function start_countdown() {
     });
 }
 
+function click_cow() {
+    $('#cow img').click(function() {
+        console.log('click cow!');
+        $(this).css({transform: 'scale(0.8)'});
+        var self = this;
+        setTimeout(function() {
+            $(self).css({transform: 'scale(1.0)'});
+        }, 800);
+    });
+}
+
 $(function() {
 //  var code = get_code();
 //  var user = null;
@@ -49,11 +60,11 @@ $(function() {
 //  });
     $(window).on('resize', function() {
         var width = $(window).width();
-        var height = 1.1 * width;
+        var height = 1.2 * width;
         $('div.container').css('min-height', height);
-        console.log('new height', height);
     });
     $(window).resize();
 
     start_countdown();
+    click_cow();
 });
